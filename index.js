f461f8c5b6747a14c0cd202e823bdefdc3dc8488
Zack Njenga <izackkiari@gmail.com>
2025-03-22 19:34:05 +0300

    Used the first two characters of the current User

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 
